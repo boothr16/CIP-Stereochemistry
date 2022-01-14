24dce00ecc16675c58245c026662f094d3b5cca1
boothr16 <boothr16@gmail.com>
2022-01-14 16:01:17 -0500

    Wrote ATOM_COMPARATOR class  for use in priority queue (min heap implementation)

---
+++ ATOM_COMPARATOR.hpp
@@ -0,0 +1,21 @@
+#ifndef ATOM_COMPARATOR_HPP
+#define ATOM_COMPARATOR_HPP
+
+#include "AtomNode.hpp"
+
+class ATOM_COMPARATOR {
+    public:
+        int operator() (
+            const AtomNode &a1,
+            const AtomNode &a2
+        );
+};
+
+int ATOM_COMPARATOR::operator(
+    const AtomNode &a1,
+    const AtomNode &a2
+) {
+    return a1.getPriority() > a2.getPriority();
+}
+
+#endif
