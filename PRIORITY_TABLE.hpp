780850d5c72c0ee5bc9b4ae99f598c5165503d8d
boothr16 <boothr16@gmail.com>
2022-01-13 20:16:19 -0500

    Designed CLI to handle wrong number of inputs and incorrect symbol syntax

---
+++ PRIORITY_TABLE.hpp
@@ -0,0 +1,20 @@
+#ifndef PRIORITY_TABLE_HPP
+#define PRIORITY_TABLE_HPP
+
+#include <unordered_map>
+#include <string>
+
+const std::unordered_map<std::string, int> PRIORITY_TABLE = {
+    {"H", 1},
+    {"C", 12},
+    {"N", 14},
+    {"O", 16},
+    {"F", 19},
+    {"P", 31},
+    {"S", 32},
+    {"Cl", 35},
+    {"Br", 79},
+    {"I", 126}
+};
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include "PRIORITY_TABLE.hpp"
+
+int main(int argc, char *argv[])
+{
+    if (argc != 5) {
+        std::cout << "Usage:"
+        << " <flat atom 1> <flat atom 2> <dashed atom>"
+        << " <wedged atom>\n";
+        return 1;
+    }
+    for (int i = 1; i < argc; i++) {
+        std::string atom = argv[i];
+        if (PRIORITY_TABLE.count(atom) == 0) {
+            std::cout << "Incorrect atomic symbol"
+            << " syntax or element not available.\n";
+            return 2;
+        }
+    }
+    return 0;
+}
