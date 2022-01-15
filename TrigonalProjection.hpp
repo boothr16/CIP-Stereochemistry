9dd25380ed6cc075bc9df435f27e128828dca12a
boothr16 <boothr16@gmail.com>
2022-01-15 17:49:31 -0500

    Added TrigonalProjection struct

---
+++ TrigonalProjection.hpp
@@ -0,0 +1,16 @@
+#ifndef TRIGONALPROJECTION_HPP
+#define TRIGONALPROJECTION_HPP
+
+#include <string>
+#include <iostream>
+
+struct TrigonalProjection {
+    AtomNode *top, *left, *right;
+    TrigonalProjection(AtomNode *minAtom);
+};
+
+TrigonalProjection::TrigonalProjection(AtomNode *minAtom) {
+    if ()
+}
+
+#endif
