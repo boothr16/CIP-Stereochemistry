6842c2c7ce408c71246a22092bdf5a2b10f9cbb2
boothr16 <boothr16@gmail.com>
2022-01-15 22:01:35 -0500

    More comments

--- AtomNode.hpp
+++ AtomNode.hpp
@@ -3,6 +3,7 @@
 
 #include "PRIORITY_TABLE.hpp"
 
+// AtomNode API
 class AtomNode {
     private:
         int prio;
@@ -12,10 +13,13 @@ class AtomNode {
         int getPriority() const;
 };
 
+// AtomNode Implementation
 AtomNode::AtomNode() : prio(-1) {}
+
 AtomNode::AtomNode(const std::string& element) {
     prio = PRIORITY_TABLE[element];
 }
+
 int AtomNode::getPriority() const {
     return prio;
 }
