d3095263ad99afb2f45c3ba6ea728fc839aced23
boothr16 <boothr16@gmail.com>
2022-01-15 19:25:06 -0500

    Output string returned from GET_STEREOCHEMISTRY in main

--- main.cpp
+++ main.cpp
@@ -8,8 +8,8 @@ int main(int argc, char *argv[])
 {
     if (argc != 5) {
         std::cout << "Usage:"
-        << " <flat atom 1> <flat atom 2> <dashed atom>"
-        << " <wedged atom>\n";
+        << " <flat atom 1> <flat atom 2> <wedged atom>"
+        << " <dashed atom>\n";
         return 1;
     }
     CentralAtom center;
@@ -23,7 +23,7 @@ int main(int argc, char *argv[])
     }
     center.buildBonds(argv[1], argv[2], argv[3], argv[4]);
     // TODO: throw exception for minAtom == nullptr. Write rotate method and determine stereochemistry.
-    TrigonalProjection tp(&center);
-    std::cout << tp.GET_STEREOCHEMISTRY(&center);
+    TrigonalProjection tp(center);
+    std::cout << tp.GET_STEREOCHEMISTRY(center);
     return 0;
 }
