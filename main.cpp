418775a67f81e5163b1c5f4d2223466c62161561
boothr16 <boothr16@gmail.com>
2022-01-17 14:06:14 -0500

    Remove debugging print statements

--- main.cpp
+++ main.cpp
@@ -16,15 +16,12 @@ int main(int argc, char *argv[])
     }
     Tetrahedral center;
     for (int i = 1; i < argc; i++) {
-        //std::string atom = makeUppercase(argv[i]);
         makeUppercase(argv[i]);
-        std::cout << argv[i] << std::endl;
         if (PRIORITY_TABLE.count(argv[i]) == 0) {
             std::cout << "Incorrect atomic symbol"
             << " syntax or element not available.\n";
             return 2;
         }
-        //argv[i] = atom;
     }
     center.buildBonds(argv[1], argv[2], argv[3], argv[4]);
     TrigonalProjection tp(center);
@@ -32,7 +29,7 @@ int main(int argc, char *argv[])
     return 0;
 }
 
-// Make user input case-insensitive
+// Make user input match case in PRIORITY_TABLE
 void makeUppercase(char *atom) {
     char *tmp = atom;
     while (*tmp) {
